fdeeccca12679c041988e883cbd7ee7621633391
Guilherme-Agency <suny.contato@gmail.com>
2026-09-08 19:45:58 -0300

    Add updater settings interleaving regression test

--- tests/SteamAuth.PhoneEnrollment.Tests/UpdateSettingsConcurrencyTests.cs
+++ tests/SteamAuth.PhoneEnrollment.Tests/UpdateSettingsConcurrencyTests.cs
@@ -1,4 +1,6 @@
 using Steam_Desktop_Authenticator;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace SteamAuth.PhoneEnrollment.Tests
@@ -27,6 +29,40 @@ namespace SteamAuth.PhoneEnrollment.Tests
                 currentRevision: 5);
 
             Assert.True(mergedValue);
+        }
+
+        [Fact]
+        public async Task SettingsSavePausedDuringProxyValidation_PreservesUpdaterDisable()
+        {
+            bool persistedCheckForUpdates = true;
+            long updatePreferenceRevision = 4;
+            TaskCompletionSource<bool> proxyValidationStarted = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            TaskCompletionSource<bool> releaseSettingsSave = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            long saveStartRevision = Volatile.Read(ref updatePreferenceRevision);
+
+            Task<bool> settingsSave = CompleteSettingsSaveAfterProxyValidationAsync();
+            await proxyValidationStarted.Task;
+
+            Volatile.Write(ref persistedCheckForUpdates, false);
+            Interlocked.Increment(ref updatePreferenceRevision);
+            releaseSettingsSave.SetResult(true);
+
+            Assert.False(await settingsSave);
+            Assert.False(Volatile.Read(ref persistedCheckForUpdates));
+
+            async Task<bool> CompleteSettingsSaveAfterProxyValidationAsync()
+            {
+                proxyValidationStarted.SetResult(true);
+                await releaseSettingsSave.Task;
+
+                bool mergedValue = MainForm.MergeCheckForUpdatesPreference(
+                    requestedValue: true,
+                    currentValue: Volatile.Read(ref persistedCheckForUpdates),
+                    saveStartRevision,
+                    currentRevision: Volatile.Read(ref updatePreferenceRevision));
+                Volatile.Write(ref persistedCheckForUpdates, mergedValue);
+                return mergedValue;
+            }
         }
     }
 }
